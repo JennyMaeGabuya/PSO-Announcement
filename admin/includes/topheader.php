a537512fd09536809beece13b6715138d715f89a
Jenny Mae A. Gabuya <jennymaegabuya8@gmail.com>
2024-08-03 20:58:53 +0800

    .

--- admin/includes/topheader.php
+++ admin/includes/topheader.php
@@ -7,14 +7,14 @@
   <ul class="nav right">
     <li class="dropdown" id="profile-messages">
       <a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle">
-        <i class="fas fa-user-circle"></i> <span class="text">Welcome Admin</span><b class="caret"></b>
+        <i class="fas fa-user-circle"></i> <span class="text"> Welcome Admin </span><b class="caret"></b>
       </a>
       <ul class="dropdown-menu">
         <li><a href="admin-profile.php"><i class="fas fa-user"></i> My Profile</a></li>
         <li class="divider"></li>
         <li><a href="change-password.php"><i class="fas fa-lock"></i> Change Password</a></li>
         <li class="divider"></li>
-        <li><a href="../logout.php"><i class="fas fa-power-off"></i> Log Out</a></li>
+        <li><a href="../logout.php"><i class="fas fa-power-off"></i> Log Out </a></li>
       </ul>
     </li>
     <li class=""><a title="" href="../logout.php"><i class="fas fa-power-off"></i> <span class="text">Logout</span></a></li>
